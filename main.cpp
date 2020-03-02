fb1df4eb7e282e2c3b2d62de0c7a22fc4cf90cb4
GeorgeBodea <61265969+GeorgeBodea@users.noreply.github.com>
2020-03-02 22:16:20 +0200

    Schimbari majore: Am aduagat aproape toate comenzile

--- main.cpp
+++ main.cpp
@@ -7,9 +7,11 @@ private:
     char info;
     Nod *next;
 public:
-    void alocare_initiala()
-    {cin>>info;
-    next=NULL;}
+    Nod()
+    {
+        cin>>info;
+        next=NULL;
+    }
 
     Nod *alocare_adresa(Nod* adresa)
     {
@@ -29,47 +31,103 @@ public:
     {
         return next;
     }
+    void afisare_informatie()
+    {
+        cout<<info<<' ';
+    }
+    Nod* adresa_nod()
+    {
+        return this;
+    }
+
 };
 
 class Coada_de_caractere
 {
-    private:
-Nod *inceput,*sfarsit;
-    public:
-Coada_de_caractere()
-{
-    inceput=NULL;
-    sfarsit=NULL;
-}
-void push()
-{
-Nod* curent=new Nod;
-if (inceput==NULL)
-    {curent->alocare_initiala();
-     inceput=curent;
-     sfarsit=curent;}
-else
+private:
+    Nod *inceput,*sfarsit;
+public:
+    Coada_de_caractere()
     {
-    sfarsit->alocare_adresa(curent);
-    curent->alocare_caracter();
-    curent->alocare_adresa(NULL);
+        inceput=NULL;
+        sfarsit=NULL;
     }
-}
+    void push()
+    {
+        Nod* curent=new Nod;
+        if (inceput==NULL)
+        {
+            //curent->alocare_initiala();
+            inceput=curent;
+            sfarsit=curent;
+        }
+        else
+        {
+            sfarsit->alocare_adresa(curent);
+            curent->alocare_caracter();
+            curent->alocare_adresa(NULL);
+        }
+    }
 
-bool isempty()
-{
-    if (inceput==NULL)
-        return true;
-    else return false;
-}
+    void pop()
+    {
+        Nod* curent=inceput;
+        if (curent==sfarsit)
+            {if (curent!=NULL)
+                delete curent;}
+        else
+            {
+                Nod* inapoi_curent=inceput;
+                curent=curent->interogare_next();
+                while(curent!=sfarsit)
+                {
+                    inapoi_curent=curent;
+                    curent=curent->interogare_next();
+                }
+                delete curent;
+                sfarsit=inapoi_curent;
+            }
+    }
+
+    bool isempty()
+    {
+        if (inceput==NULL)
+            return true;
+        else
+            return false;
+    }
+
+    Nod* interogare_adresa_inceput()
+        {return inceput->adresa_nod();}
+
+    friend ostream &operator <<(ostream& output,Coada_de_caractere &coada);
 
 };
+
+ostream& operator <<(ostream& output,Coada_de_caractere &coada)
+    {
+        Nod* curent=coada.inceput;
+        while(curent!=coada.sfarsit)
+        {
+            output<<curent->interogare_info(); // Inainte: curent->afisare_informatie(); Problema.
+            delete curent;
+            curent=curent->interogare_next();
+        }
+        output<< curent->interogare_info(); // Inainte: curent->afisare_informatie();
+        //delete curent;
+        return output;
+    }
 
 
 int main()
 {
     Coada_de_caractere coada;
-    //coada.push();
-    //coada.push();
+    coada.push();
+    coada.push();
+    Nod* p1,p2;
+    p1=coada.interogare_adresa_inceput();
+    cout<<p1->interogare_info();
+
+    //cout<<coada;
     return 0;
 }
