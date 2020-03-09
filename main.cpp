16047ea79fd8b7a3d014a26d64eacfd21645786a
GeorgeBodea <61265969+GeorgeBodea@users.noreply.github.com>
2020-03-09 16:38:34 +0200

    Rezolvat bug de afisare.
    Testat cu Clion si CodeBlocks.

--- main.cpp
+++ main.cpp
@@ -13,18 +13,13 @@ public:
         next = NULL;
     }
 
-    Nod(char informatie)
+    explicit Nod(char informatie)
     {
         info = informatie;
         next = NULL;
     }
 
-    Nod(int i)
-    {
-        next = NULL;
-    }
-    
-    Nod *alocare_adresa(Nod* adresa)
+    void alocare_adresa(Nod* adresa)
     {
         next = adresa;
     }
@@ -37,11 +32,6 @@ public:
     Nod* interogare_next()
     {
         return next;
-    }
-
-    void afisare_informatie()
-    {
-        cout << info << ' ';
     }
 
 };
@@ -66,10 +56,10 @@ public:
             sfarsit -> alocare_adresa(NULL);
         }
         else
-            {
-                sfarsit -> alocare_adresa(curent);
-                sfarsit = curent;
-            }
+        {
+            sfarsit -> alocare_adresa(curent);
+            sfarsit = curent;
+        }
 
     }
 
@@ -93,93 +83,86 @@ public:
     {
         Nod* curent = inceput;
         if (curent == sfarsit)
-            {
-                if (curent != NULL)
-                    delete curent;
-                inceput = NULL;
-                sfarsit = NULL;
-            }
+        {
+            //if (curent != nullptr)
+            //    delete curent;
+            inceput = NULL;
+            sfarsit = NULL;
+        }
         else
-            {   inceput = inceput -> interogare_next();
-                delete curent;
-            }
+        {   inceput = inceput -> interogare_next();
+            delete curent;
+        }
     }
 
     bool vida()
     {
-        if (inceput == NULL)
-            return true;
-        else
-            return false;
+        return inceput == NULL;
     }
 
-Coada_de_caractere operator + (Coada_de_caractere &coada_de_adaugat)
+    Coada_de_caractere operator + (Coada_de_caractere &coada_de_adaugat)
     {
-         Coada_de_caractere coada_finala;
-         Nod* adresa_ob1 = inceput;
-         while(adresa_ob1 != NULL) // adaugam componentele din coada curenta
-         {
+        Coada_de_caractere coada_finala;
+        Nod* adresa_ob1 = inceput;
+        while(adresa_ob1 != NULL) // adaugam componentele din coada curenta
+        {
 
-             coada_finala.adaugare(adresa_ob1 -> interogare_info()); // Luam caracterul dau schimbam adresa
-             adresa_ob1 = adresa_ob1 -> interogare_next();
-         }
-         Nod* adresa_ob2 = coada_de_adaugat.inceput;
-         while(adresa_ob2 != NULL)
-         {
-             coada_finala.adaugare(adresa_ob2->interogare_info());
-             adresa_ob2 = adresa_ob2 -> interogare_next();
-         }
+            coada_finala.adaugare(adresa_ob1 -> interogare_info()); // Luam caracterul dau schimbam adresa
+            adresa_ob1 = adresa_ob1 -> interogare_next();
+        }
+        Nod* adresa_ob2 = coada_de_adaugat.inceput;
+        while(adresa_ob2 != NULL)
+        {
+            coada_finala.adaugare(adresa_ob2->interogare_info());
+            adresa_ob2 = adresa_ob2 -> interogare_next();
+        }
 
-         return coada_finala;
+        return coada_finala;
     }
 
 
     Coada_de_caractere operator - (Coada_de_caractere &coada_de_adaugat)
     {
-         Coada_de_caractere coada_finala;
-         Nod* adresa_ob1 = inceput;
-         Nod* adresa_ob2 = coada_de_adaugat.inceput;
-         char aux1,aux2;
-         while(adresa_ob1 != NULL && adresa_ob2 != NULL)
-         {
-             aux1 = adresa_ob1 -> interogare_info();
-             aux2 = adresa_ob2 -> interogare_info();
-             if (aux1 > aux2)
-                coada_finala.adaugare(aux1);
-             else
-                coada_finala.adaugare(aux2);
-             // this -> elimina();
-             // coada_de_adaugat.elimina();
-             adresa_ob1 = adresa_ob1->interogare_next();
-             adresa_ob2 = adresa_ob2->interogare_next();
-         }
-
-         return coada_finala;
-    }
-
-    Nod* interogare_adresa_inceput()
+        Coada_de_caractere coada_finala;
+        Nod* adresa_ob1 = inceput;
+        Nod* adresa_ob2 = coada_de_adaugat.inceput;
+        char aux1,aux2;
+        while(adresa_ob1 != NULL && adresa_ob2 != NULL)
         {
-            return inceput;
+            aux1 = adresa_ob1 -> interogare_info();
+            aux2 = adresa_ob2 -> interogare_info();
+            if (aux1 > aux2)
+                coada_finala.adaugare(aux1);
+            else
+                coada_finala.adaugare(aux2);
+            // this -> elimina();
+            // coada_de_adaugat.elimina();
+            adresa_ob1 = adresa_ob1->interogare_next();
+            adresa_ob2 = adresa_ob2->interogare_next();
         }
 
+        return coada_finala;
+    }
     friend ostream &operator << (ostream& output,Coada_de_caractere &coada);
     friend istream &operator >> (istream& input,Coada_de_caractere &coada);
 
 };
 
-ostream& operator << (ostream& output,Coada_de_caractere &coada)
+ostream& operator << (ostream& output, Coada_de_caractere &coada)
+{
+    Nod* curent = coada.inceput;
+    //Nod* aux;
+    while(curent != NULL)
     {
-        Nod* curent = coada.inceput;
-        while(curent != NULL) //
-        {
-            output << curent -> interogare_info()<<' '; // Inainte: curent->afisare_informatie(); Problema.
-            delete curent;
-            curent = curent -> interogare_next();
-        }
-        return output;
+        output << curent -> interogare_info()<<' '; // Inainte: curent->afisare_informatie(); Problema.
+        //aux=curent;
+        //delete curent;
+        curent = curent -> interogare_next();
     }
+    return output;
+}
 
-    istream & operator >> (istream &input,  Coada_de_caractere &coada) //putem schimba numele parametrilor fata de prototip?
+istream & operator >> (istream &input,  Coada_de_caractere &coada) //putem schimba numele parametrilor fata de prototip?
 {
     char caracter;
     input >> caracter;
@@ -187,7 +170,7 @@ ostream& operator << (ostream& output,Coada_de_caractere &coada)
     return input;
 }
 
- Coada_de_caractere& operator >> (Coada_de_caractere &coada,  char informatie) //putem schimba numele parametrilor fata de prototip?
+Coada_de_caractere& operator >> (Coada_de_caractere &coada,  char informatie) //putem schimba numele parametrilor fata de prototip?
 {
     coada.adaugare(informatie);
     return coada;
@@ -197,19 +180,18 @@ ostream& operator << (ostream& output,Coada_de_caractere &coada)
 
 int main()
 {
+
+
     Coada_de_caractere coada1, coada2, coada3, coada4;
     coada1 >> 'a' >> 'b';
     coada2.adaugare('c');
-
     coada3 = coada1 + coada2;
     cout << "coada1 + coada2 = " << coada3 << '\n';
-
     coada4 = coada2 - coada1;
     cout << "coada2 - coada1 = " << coada4 << '\n';
-
-
     cout << "Dati un caracter: ";
     cin >> coada4;
-    cout << "coada4 = "<<coada4;
+    cout <<  coada4;
+    cout << coada4.vida();
     return 0;
 }
