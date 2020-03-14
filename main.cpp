29ddb4e3668312b53d9bbe375e8de86dd0124e13
GeorgeBodea <61265969+GeorgeBodea@users.noreply.github.com>
2020-03-14 13:55:24 +0200

    Am modificat functia de adaugare.
    In legatura cu identarea, folosesc formatul Allman (ANSI), primul din Settings -> Editor -> Source formatter. Nu vad inconsistente, codul este formatat cum trebuie dupa acest Standard.

--- main.cpp
+++ main.cpp
@@ -53,13 +53,13 @@ public:
         {
             inceput = curent;
             sfarsit = curent;
-            sfarsit -> alocare_adresa(NULL);
         }
         else
         {
             sfarsit -> alocare_adresa(curent);
             sfarsit = curent;
         }
+        sfarsit -> alocare_adresa(NULL);
 
     }
 
@@ -70,13 +70,13 @@ public:
         {
             inceput = curent;
             sfarsit = curent;
-            sfarsit -> alocare_adresa(NULL);
         }
         else
         {
             sfarsit -> alocare_adresa(curent);
             sfarsit = curent;
         }
+        sfarsit -> alocare_adresa(NULL);
     }
 
     void elimina()
@@ -84,13 +84,12 @@ public:
         Nod* curent = inceput;
         if (curent == sfarsit)
         {
-            //if (curent != nullptr)
-            //    delete curent;
             inceput = NULL;
             sfarsit = NULL;
         }
         else
-        {   inceput = inceput -> interogare_next();
+        {
+            inceput = inceput -> interogare_next();
             delete curent;
         }
     }
@@ -100,28 +99,26 @@ public:
         return inceput == NULL;
     }
 
-    Coada_de_caractere operator + (Coada_de_caractere &coada_de_adaugat)
+    Coada_de_caractere operator + (const Coada_de_caractere &coada_de_adaugat)
     {
         Coada_de_caractere coada_finala;
         Nod* adresa_ob1 = inceput;
         while(adresa_ob1 != NULL) // adaugam componentele din coada curenta
         {
-
             coada_finala.adaugare(adresa_ob1 -> interogare_info()); // Luam caracterul dau schimbam adresa
             adresa_ob1 = adresa_ob1 -> interogare_next();
         }
         Nod* adresa_ob2 = coada_de_adaugat.inceput;
         while(adresa_ob2 != NULL)
         {
-            coada_finala.adaugare(adresa_ob2->interogare_info());
+            coada_finala.adaugare(adresa_ob2 -> interogare_info());
             adresa_ob2 = adresa_ob2 -> interogare_next();
         }
-
         return coada_finala;
     }
 
 
-    Coada_de_caractere operator - (Coada_de_caractere &coada_de_adaugat)
+    Coada_de_caractere operator - (Coada_de_caractere &coada_de_adaugat) // problema: se apeleaza destructorul cand avem =
     {
         Coada_de_caractere coada_finala;
         Nod* adresa_ob1 = inceput;
@@ -143,66 +140,58 @@ public:
 
         return coada_finala;
     }
-    
+
     ~ Coada_de_caractere()
     {
+
         Nod* curent=inceput;
         Nod* aux;
         while(curent!=NULL)
         {
-        aux=curent;
-        curent = curent->interogare_next();
-        delete aux;
+            aux=curent;
+            curent = curent->interogare_next();
+            delete aux;
         }
-
     }
-    
-    friend ostream &operator << (ostream& output,Coada_de_caractere &coada);
+
+    friend ostream &operator << (ostream& output,const Coada_de_caractere &coada);
     friend istream &operator >> (istream& input,Coada_de_caractere &coada);
 
 };
 
-ostream& operator << (ostream& output, Coada_de_caractere &coada)
+ostream& operator << (ostream& output,const Coada_de_caractere &coada)
 {
     Nod* curent = coada.inceput;
-    //Nod* aux;
     while(curent != NULL)
     {
         output << curent -> interogare_info()<<' '; // Inainte: curent->afisare_informatie(); Problema.
-        //aux=curent;
-        //delete curent;
         curent = curent -> interogare_next();
     }
     return output;
 }
 
-istream & operator >> (istream &input,  Coada_de_caractere &coada) //putem schimba numele parametrilor fata de prototip?
+istream & operator >> (istream &input,  Coada_de_caractere &coada)
 {
     char caracter;
     input >> caracter;
     coada.adaugare(caracter);
-    return input;
+    return input; // Pentru a folosii >> de mai multe ori pe aceasi linie
 }
 
-Coada_de_caractere& operator >> (Coada_de_caractere &coada,  char informatie) //putem schimba numele parametrilor fata de prototip?
+Coada_de_caractere& operator >> (Coada_de_caractere &coada,  char informatie)
 {
     coada.adaugare(informatie);
     return coada;
 }
 
-
-
 int main()
 {
-
 
     Coada_de_caractere coada1, coada2, coada3, coada4;
     coada1 >> 'a' >> 'b';
     coada2.adaugare('c');
-    coada3 = coada1 + coada2;
-    cout << "coada1 + coada2 = " << coada3 << '\n';
-    coada4 = coada2 - coada1;
-    cout << "coada2 - coada1 = " << coada4 << '\n';
+    cout << coada1 + coada2 << '\n';
+    cout << "coada2 - coada1 = " <<  coada2 - coada1 << '\n';
     cout << "Dati un caracter: ";
     cin >> coada4;
     cout <<  coada4;
